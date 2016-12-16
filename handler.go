bb1b50cb9f70c79fdee841725273de9c26bb4b86
tobique <tobique@users.noreply.github.com>
2016-12-16 22:47:06 +0100

    Fix imports, execute delayed

--- handler.go
+++ handler.go
@@ -1,14 +1,15 @@
 package podca_api
 
-
-
 import(
 	"net/http"
-	"github.com/golang/appengine"
 	"encoding/json"
-	"github.com/golang/appengine/log"
 	"github.com/gorilla/mux"
 	"strconv"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/delay"
+
+	"golang.org/x/net/context"
 )
 
 func rootHandler() http.HandlerFunc {
@@ -25,6 +26,7 @@ func podcastSearchHandler(searchEngine *ITunesSearchEngine, worker *FeedWorker) 
 	//var laterFunc = delay.Func("key", worker.Retrieve())
 
 	return func(w http.ResponseWriter, r *http.Request) {
+
 		ctx := appengine.NewContext(r)
 		var term = r.FormValue("term")
 		podcasts, err := searchEngine.Search(ctx, term) ; if err != nil {
@@ -32,7 +34,11 @@ func podcastSearchHandler(searchEngine *ITunesSearchEngine, worker *FeedWorker) 
 		}
 
 		for _, podcast := range podcasts {
-			worker.Retrieve(ctx, podcast)
+			var delayedWorker = delay.Func("feedWorker", func(ctx context.Context, podcast Podcast){
+				worker.Retrieve(ctx, podcast)
+			})
+
+			delayedWorker.Call(ctx, podcast)
 
 		}
 
@@ -46,46 +52,12 @@ func podcastSearchHandler(searchEngine *ITunesSearchEngine, worker *FeedWorker) 
 }
 
 
-/*func feedParseHandler(feedRepo *FeedRepo) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		ctx := appengine.NewContext(r)
-		var feedUrl = "http://www1.wdr.de/radio/podcasts/wdr5/zeitzeichen244.podcast"
-
-		data, fetcherr := fetchData(ctx, feedUrl); if fetcherr != nil {
-			panic(fetcherr)
-		}
-
-		type Rss struct {
-			Feed	Feed `xml:"channel"`
-		}
-
-		var rss Rss
-
-		xmlerr := xml.Unmarshal(data, &rss); if xmlerr != nil {
-			panic(xmlerr)
-			return
-		}
-		feedRepo.save(ctx, &rss.Feed)
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		if jsonerr := json.NewEncoder(w).Encode(rss.Feed); jsonerr != nil {
-			panic(jsonerr)
-		}
-	}
-
-}*/
-
-
-
 func feedHandler(feedRepo *FeedRepo) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		switch r.Method {
 		case "GET":
-
-			// Get all and return
-				ctx := appengine.NewContext(r)
-
+			ctx := appengine.NewContext(r)
 			vars := mux.Vars(r)
 
 			feedId, _ := strconv.Atoi(vars["feedId"])
@@ -98,7 +70,7 @@ func feedHandler(feedRepo *FeedRepo) http.HandlerFunc {
 					panic(jsonerr)
 				}
 			} else {
-				result, _ :=feedRepo.getAll(ctx)
+				result, _ :=feedRepo.getAllOverview(ctx)
 				w.WriteHeader(http.StatusOK)
 				w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 				if jsonerr := json.NewEncoder(w).Encode(result); jsonerr != nil {
@@ -118,7 +90,6 @@ func feedHandler(feedRepo *FeedRepo) http.HandlerFunc {
 
 		}
 
-		//w.Write([]byte("API Podca "))
 	}
 }
 
